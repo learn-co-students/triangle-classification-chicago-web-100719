e90e0a5e0f39b36cb0a2305bad624f4db9ee7fe4
Daniel Ionita <danielionita@Daniels-MacBook-Pro.local>
2019-10-16 21:15:34 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_reader :num1, :num2, :num3
+
+  def initialize(num1, num2, num3)
+   @num1 = num1
+   @num2 = num2
+   @num3 = num3
+   
+  end
+
+  def kind
+    if @num1 <= 0 || @num2 <= 0 || @num3 <= 0 || [@num1, num2].sum <= @num3 || [@num1, @num3].sum <= @num2 || [@num2, @num3].sum <= @num1
+      raise TriangleError
+    elsif @num1 == @num2 && @num1 == @num3               
+      :equilateral
+    elsif @num1 == @num2 || @num1 == @num3 || @num2 == @num3  
+      :isosceles
+    elsif @num1 != @num2 && @num2 != @num3 && @num1 != @num3
+      :scalene                         
+    end
+    
+  end
+
+  class TriangleError < StandardError
+
+  end
+
 end
