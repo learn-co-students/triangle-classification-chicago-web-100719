f2f704aa70d3e1553b9ad176610125a8cdb8fb01
bosmanpa <bsmn.ptrck@gmail.com>
2019-10-16 22:40:21 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c 
+
+  def initialize (a, b, c)
+  @a = a
+  @b = b
+  @c = c
+  end
+
+  def kind
+    if( 
+      a <= 0 || b <=0 || c <=0 || (a + b) <= c || (a + c) <= b || (b + c) <= a
+    )
+      begin
+        raise TriangleError
+      # rescue TriangleError => error
+      #   puts error.message
+      end
+    elsif a == b && b == c
+      :equilateral
+
+    elsif a == b && b != c || a == c  && c != b || b == c && b != a
+      :isosceles
+
+    elsif a != b && b != c && a != c
+      :scalene
+    end
+
+
+
+
+
+
+
+
+  end
+
+  class TriangleError < StandardError
+    def message 
+    "ARE YOU TRYING TO ALTER REALITY?!"
+    end
+  end
+
 end
