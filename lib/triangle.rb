837a78d80b7f7770f7af07c4c6b5d303ff46a2a1
NaveedG <nganjani@protonmail.com>
2019-10-20 10:52:55 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    lengths = [@x, @y, @z]
+    if (lengths.any? { |x| x <= 0 }) || (@x >= @y + @z) || (@y >= @x + @z) || (@z >= @x + @y)
+      raise TriangleError
+    else
+      if @x == @y && @y == @z
+        return :equilateral
+      elsif (@x == @y && @y != @z) || (@y == @z && @x != @y) || (@x == @z && @x != @y)
+       return :isosceles
+      else
+       return :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
